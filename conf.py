3cc42fd911a5e73668f6f4142de273ff4fba794a
Abhipsha Das <abhipsha.das16@gmail.com>
2021-06-03 19:41:21 +0530

    add files

---
+++ conf.py
@@ -0,0 +1,100 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'musings of a hermit crab'
+copyright = '2021, Abhipsha Das'
+author = 'Abhipsha Das'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "myst_nb",
+    "ablog",
+    "sphinx_panels",
+    "sphinxext.opengraph",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "*import_posts*", "**/pandoc_ipynb/inputs/*"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'pydata_sphinx_theme'
+
+html_theme_options = {
+  "github_url": "https://github.com/chiral-carbon/",
+  "search_bar_text": "Search this site...",
+  "search_bar_position": "navbar",
+}
+
+html_favicon = "_static/favicon.ico"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_extra_path = ["feed.xml"]
+html_sidebars = {
+    "index": ["hello.html"],
+    "about": ["hello.html"],
+    "publications": ["hello.html"],
+    "projects": ["hello.html"],
+    "talks": ["hello.html"],
+    "posts/**": ['postcard.html', 'recentposts.html', 'archives.html'],
+    "blog": ['tagcloud.html', 'archives.html'],
+    "blog/**": ['postcard.html', 'recentposts.html', 'archives.html']
+}
+blog_baseurl = "https://chiral-carbon.github.io"
+blog_title = "musings of a hermit crab"
+blog_path = "blog"
+fontawesome_included = True
+blog_post_pattern = "posts/*/*"
+post_redirect_refresh = 1
+post_auto_image = 1
+post_auto_excerpt = 2
+
+# Panels config
+panels_add_bootstrap_css = False
+
+# MyST config
+myst_admonition_enable = True
+myst_deflist_enable = True
+
+# OpenGraph config
+ogp_site_url = "https://chiral-carbon.github.io"
+ogp_image = "https://chiral-carbon.github.io/_static/profile.jpg"
+
+# Temporarily stored as off until we fix it
+jupyter_execute_notebooks = "off"
+
+def setup(app):
+    app.add_css_file("custom.css")
---
+++ index.md
@@ -0,0 +1,31 @@
+# Chris Holdgraf - about me
+
+Hey there 👋
+
+I work at the intersection of technical development, open communities, scientific research, and education.
+
+I also help run [2i2c - the International Interactive Computing Collaboration](https://2i2c.org), a non-profit dedicated to open source infrastructure for interactive computing in research and education.
+
+I work with teams to create and improve open-source technology for scientists, educators, and data analysts. My goal is to help people do their work more effectively, openly, inclusively, and reproducibly.
+
+You can learn a bit more about what I do on this website.
+
+## Recent posts
+
+See the [blog archives](blog) for a more complete list.
+
+```{postlist}
+:date: "%Y-%m-%d"
+:format: "{date} - {title}"
+:excerpts:
+```
+
+```{toctree}
+:maxdepth: 2
+:hidden:
+about
+projects
+publications
+talks
+blog
+```
